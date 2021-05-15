3c8972161cbe81b0d0202ca05bd10443184d6599
Purva4785 <77827137+Purva4785@users.noreply.github.com>
2021-05-15 16:29:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<HTML>
+<head>
+	 <meta name="author" content="Vinit Shahdeo">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <meta name="format-detection" content="telephone=no">
+      
+	
+      <link rel = "stylesheet" type = "text/css" href = "buttons.css">
+    <style>
+    .t {
+			font-family: Georgia;
+			border: 2px solid white;
+		    border-radius: 1px;
+		    background-color: white;
+			opacity:0.7;
+		    text-align: center;
+		    padding: 10px;
+
+		}
+		.btn1,.btn2 {
+			background-color:white;
+			
+			border:0px;
+			color:darkblue;
+			font-size: 20px;
+			font-weight: bold;
+			font-family: Arial, Helvetica, sans-serif;
+		}
+        body{
+            background-image: url("home.jpg");
+            background-size: cover;
+        }
+		.four,.three{
+			color:white;
+			width:1350px;
+			background-color:black;
+			opacity:0.5;
+			box-sizing: content-box;
+		}
+		p{
+			background-color:wheat;
+			margin:20px;
+			text-align: center;
+			font-size: 25px;
+			font-family: Arial, Helvetica, sans-serif;
+
+		}
+    </style>
+</head>
+<body>
+	
+
+<link rel = "stylesheet" type = "text/css" href = "style.css">
+
+
+	<div class="one" style=" background-color: #b3ecff;
+   height: :50px;
+   width:100%;">
+		<table width="1330">
+            		
+			<table width="1200" align = "center"  class = "t">
+                <tr>
+				<td style = "text-align:center"> <button class = "btn1"> Home </button></td> 	
+				<td style = "text-align:center"><a href="customer.php" target="frame"><button class = "btn2">
+                    View Customers</button></a></td>
+				<td style = "text-align:center"><a href="Transfermoney.php" target="frame"><button class = "btn2">
+                    Digital Money Transfer</button></a></td>
+				<td style = "text-align:center"><a href="transactionhistory.php" target="frame"><button class = "btn2">
+                    View Transaction History</button></a></td>
+                </tr>
+            </table>
+        </table>
+	</div>
+	
+	<div class="two">Welcome to</div>
+	<h1 >
+	<u>THE SPARK FOUNDATION BANK</u></h1><br>
+
+	<div class="three" ><b><U> Banking System that helps to transfer money between users </U></b><br></div>
+
+    <div class="four"> Connect with us on social media channels:</div> 
+
+		 <p><i class="fa fa-github" style="font-size:25px;color:black;font-family:Arial"></i>
+		 <a href="https://github.com/Purva4785">    #GitHub     </a><BR>
+         <i class="fa fa-linkedin" style="font-size:25px;color:black">
+		 <a href="https://in.linkedin.com/in/purva-virkar-2a4921204">#Linkedin</a></p>
+        
+	</div>
+</body>
+</html>
